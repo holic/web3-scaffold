aa75068a13e0f1b880e8a8fc91d51fc21a4df59f
Kevin Ingersoll <kingersoll@gmail.com>
2022-02-18 23:56:31 -0800

    improve deploy script

--- packages/contracts/scripts/deploy.ts
+++ packages/contracts/scripts/deploy.ts
@@ -1,10 +1,8 @@
-import "@nomiclabs/hardhat-ethers";
-
 import { ContractFactory } from "ethers";
 import fs from "fs/promises";
 import hre, { ethers, network } from "hardhat";
 
-import { ExampleNFT__factory } from "../typechain-types";
+import { ExampleNFT, ExampleNFT__factory } from "../typechain-types";
 
 const exists = (path: string) =>
   fs
@@ -12,7 +10,14 @@ const exists = (path: string) =>
     .then(() => true)
     .catch(() => false);
 
-const deployContract = async (factory: ContractFactory) => {
+// TODO: add type for deploys JSON
+
+const contractsToRedeploy = process.env.REDEPLOY_CONTRACTS?.split(",") || [];
+
+const deployContract = async <T extends ContractFactory>(
+  factory: T,
+  args: Parameters<T["deploy"]>
+) => {
   const deploysPath = `${__dirname}/../deploys.json`;
   const deploys = (await exists(deploysPath))
     ? JSON.parse((await fs.readFile(deploysPath)).toString())
@@ -21,21 +26,33 @@ const deployContract = async (factory: ContractFactory) => {
   const deployedContracts = deploys[network.name] || {};
   const contractName = factory.constructor.name.replace(/__factory$/, "");
 
-  if (deployedContracts[contractName] && !process.env.REDEPLOY_CONTRACTS) {
-    throw new Error(
-      `Contract ${contractName} has already been deployed to ${deployedContracts[contractName].address}. Use \`REDEPLOY_CONTRACTS=1\` to deploy a new contract and override the stored address.`
+  if (
+    deployedContracts[contractName] &&
+    !contractsToRedeploy.includes(contractName)
+  ) {
+    const { address } = deployedContracts[contractName];
+    console.log(
+      `Contract ${contractName} has already been deployed to ${address}. Use \`REDEPLOY_CONTRACTS=${contractName}\` to deploy a new contract and override the stored address.`
     );
+    return factory.attach(address);
   }
 
-  const contract = await factory.deploy();
+  const contract = await factory.deploy(...args);
   // For some reason, `contract.address` is returning a different address
   // than the one deployed. Might be a Polygon issue?
   //
   // Instead, grab `contractAddress` from the deploy transaction receipt.
   const tx = await contract.deployTransaction.wait();
   console.log(
-    `${contractName} deployed at ${tx.contractAddress} (block #${tx.blockNumber})`
+    `${contractName} deployed at ${tx.contractAddress} (block #${
+      tx.blockNumber
+    }), cost: ${ethers.utils.formatUnits(
+      tx.gasUsed.mul(tx.effectiveGasPrice),
+      18
+    )}`
   );
+
+  // TODO: consider storing args? and use that to determine if we should redeploy?
 
   await fs.writeFile(
     deploysPath,
@@ -63,14 +80,22 @@ const deployContract = async (factory: ContractFactory) => {
     address: tx.contractAddress,
     constructorArguments: [],
   });
+
+  return factory.attach(tx.contractAddress);
 };
 
-async function start() {
+const start = async () => {
   const [deployer] = await ethers.getSigners();
-  await deployContract(new ExampleNFT__factory(deployer));
+  console.log(`Starting deploy on ${network.name} from ${deployer.address}`);
+
+  // TODO: figure out how to get TS to infer contract type so we don't have to cast
+  const exampleNFT = (await deployContract(
+    new ExampleNFT__factory(deployer),
+    []
+  )) as ExampleNFT;
 
   console.log("Done!");
-}
+};
 
 start().catch((e: Error) => {
   console.error(e);
