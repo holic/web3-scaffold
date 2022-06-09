8ca09b9c09c73e24d57404afd98624989f427de4
Kevin Ingersoll <kingersoll@gmail.com>
2022-06-09 16:31:32 -0700

    example contract read

--- packages/app/src/pages/index.tsx
+++ packages/app/src/pages/index.tsx
@@ -1,8 +1,26 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useContractRead } from "wagmi";
+
+import { exampleNFTContract } from "../contracts";
 
 const HomePage: NextPage = () => {
+  const totalSupply = useContractRead(
+    {
+      addressOrName: exampleNFTContract.address,
+      contractInterface: exampleNFTContract.interface,
+    },
+    "totalSupply"
+  );
+  const maxSupply = useContractRead(
+    {
+      addressOrName: exampleNFTContract.address,
+      contractInterface: exampleNFTContract.interface,
+    },
+    "MAX_SUPPLY"
+  );
+
   return (
     <>
       <Head>
@@ -13,10 +31,12 @@ const HomePage: NextPage = () => {
         <div className="self-end p-2">
           <ConnectButton />
         </div>
-        <div className="flex-grow flex flex-col gap-4 items-center justify-center pb-[25vh]">
+        <div className="flex-grow flex flex-col gap-4 items-center justify-center p-8 pb-[50vh]">
           <h1 className="text-4xl">Example NFT</h1>
-          <p>sup</p>
-          <p>do yo thang</p>
+          <p>
+            {totalSupply.data?.toNumber().toLocaleString() ?? "??"}/
+            {maxSupply.data?.toNumber().toLocaleString() ?? "??"} minted
+          </p>
         </div>
       </div>
     </>
