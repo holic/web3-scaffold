068fe3faccae17c802022631c77a5b5fc1b4d576
Kevin Ingersoll <kingersoll@gmail.com>
2022-06-09 18:45:09 -0700

    update subgraph to goerli, use ABI from contracts package

--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -1,11 +1,11 @@
 import { ExampleNFT, Transfer } from "../generated/ExampleNFT/ExampleNFT";
-import { NFT } from "../generated/schema";
+import { Token } from "../generated/schema";
 
 export function handleTransfer(event: Transfer): void {
   const contract = ExampleNFT.bind(event.address);
 
-  const nft = new NFT(event.params.tokenId.toString());
-  nft.owner = event.params.to;
-  nft.tokenURI = contract.tokenURI(event.params.tokenId);
-  nft.save();
+  const token = new Token(event.params.tokenId.toString());
+  token.owner = event.params.to;
+  token.tokenURI = contract.tokenURI(event.params.tokenId);
+  token.save();
 }
