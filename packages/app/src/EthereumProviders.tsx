dcc64dbda9244202bb948836e22a401e1044a399
Davis Shaver <davisshaver@gmail.com>
2022-06-25 16:52:00 -0400

    feat: specify chain by slug instead of id

--- packages/app/src/EthereumProviders.tsx
+++ packages/app/src/EthereumProviders.tsx
@@ -5,10 +5,17 @@ import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
-export const targetChainId = parseInt(process.env.CHAIN_ID!) || 5;
+const targetChainSlug = process.env
+  .NEXT_PUBLIC_CHAIN_SLUG as keyof typeof chain;
+
+const targetChain = chain[targetChainSlug]
+  ? chain[targetChainSlug]
+  : chain.goerli;
+
+export const targetChainId = targetChain.id;
 
 export const { chains, provider, webSocketProvider } = configureChains(
-  targetChainId === 1 ? [chain.mainnet] : [chain.goerli],
+  [targetChain],
   [
     alchemyProvider({ alchemyId: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY! }),
     publicProvider(),
