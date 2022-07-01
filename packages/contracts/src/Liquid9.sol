fcf008069302af6de6e116fbef4bb5e6a6f420c8
Jason <Jason@wildcards.world>
2022-07-01 18:58:28 +0200

    add cloud9 contract, make compile, lfg

--- packages/contracts/src/Liquid9.sol
+++ packages/contracts/src/Liquid9.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
-import "hardhat/console.sol";
+import "forge-std/console.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
