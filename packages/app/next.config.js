e08bad47b2592590db04e4d8243c15763f6265ee
jborichevskiy <jborichevskiy@gmail.com>
2022-07-04 17:29:08 -0600

    try another build

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -4,12 +4,8 @@ module.exports = {
   webpack5: true,
   webpack: (config) => {
     config.resolve.fallback = {
+      ...config.resolve.fallback,
       fs: false,
-      process: false,
-      util: false,
-      zlib: false,
-      assert: false,
-      stream: false,
     };
 
     return config;
