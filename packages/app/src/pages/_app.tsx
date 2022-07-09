41ebf56b6f47ff2812e75e436902fde4e4a38863
jborichevskiy <jborichevskiy@gmail.com>
2022-07-09 11:31:34 -0600

    update favicons and title

--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -23,9 +23,44 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       <Head>
         <title>Daily Canvas</title>
         <meta
+          name="description"
+          content="One new canvas, everyday, for your pixels."
+        />
+        <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0"
         ></meta>
+
+        <link rel="shortcut icon" href="/static/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/static/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/static/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/static/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/static/favicon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/static/favicon-512x512.png"
+        />
       </Head>
       <GraphProvider value={graphClient}>
         <EthereumProviders>
