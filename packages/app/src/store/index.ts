3eefba2c35f93e5b8f4b2b1400b584543f42cebb
Arthur Tyukayev <github_arthur@tyukayev.com>
2022-07-09 13:40:13 -0700

    cleaned up linting

--- packages/app/src/store/index.ts
+++ packages/app/src/store/index.ts
@@ -2,11 +2,12 @@ import createStore from "zustand";
 import { persist } from "zustand/middleware";
 
 type State = {
+  // eslint-disable-next-line
   requests: Partial<Record<string, any>>;
 };
 
 const useStore = createStore<State>(
-  persist((set) => ({ requests: {} }), { name: "urql-cache" })
+  persist(() => ({ requests: {} }), { name: "urql-cache" })
 );
 
 export default useStore;
