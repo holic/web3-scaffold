6b9746558e288b382af39a1cb05486fdd9905659
Arthur Tyukayev <github_arthur@tyukayev.com>
2022-07-09 11:56:41 -0700

    Adjusted desktop and mobile style/visibility

--- packages/app/tailwind.config.js
+++ packages/app/tailwind.config.js
@@ -7,19 +7,6 @@ module.exports = {
     fontFamily: {
       mono: ["pixeloid"],
     },
-    screens: {
-      "2xl": { max: "1535px" },
-
-      xl: { max: "1279px" },
-
-      lg: { max: "1023px" },
-
-      md: { max: "767px" },
-
-      sm: { max: "639px" },
-
-      xs: { max: "390px" },
-    },
     extend: {},
   },
   plugins: [],
