fa1aeb30895689814c42b43328334b07be9be69f
therightchoyce <chris@therightchoyce.com>
2022-07-26 17:29:49 -0400

    Update EthereumProviders to allow for other testnets (#51)
    * Update EthereumProviders to allow for other testnets
    
    * Refacator code based for better chain selection
    
    * Remove CHAIN_ID in favor of NEXT_PUBLIC_CHAIN_ID

--- packages/app/src/EthereumProviders.tsx
+++ packages/app/src/EthereumProviders.tsx
@@ -1,14 +1,28 @@
 import "@rainbow-me/rainbowkit/styles.css";
 
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import {
+  chain,
+  configureChains,
+  createClient,
+  defaultChains,
+  WagmiConfig,
+} from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
-export const targetChainId = parseInt(process.env.CHAIN_ID || "0") || 5;
+// Will default to goerli if nothing set in the ENV
+export const targetChainId =
+  parseInt(process.env.NEXT_PUBLIC_CHAIN_ID || "0") || 5;
+
+// filter down to just mainnet + optional target testnet chain so that rainbowkit can tell
+// the user to switch network if they're on an alternative one
+const targetChains = defaultChains.filter(
+  (chain) => chain.id === 1 || chain.id === targetChainId
+);
 
 export const { chains, provider, webSocketProvider } = configureChains(
-  [chain.mainnet, chain.goerli],
+  targetChains,
   [
     alchemyProvider({ alchemyId: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY }),
     publicProvider(),
