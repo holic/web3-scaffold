a7a08d2af56e004f136fac9595ca34b887ef8b63
Arthur Tyukayev <github_arthur@tyukayev.com>
2022-08-29 23:09:49 -0700

    Updates the grid generation and fixes issues with tile selection

--- packages/app/src/pages/history.tsx
+++ packages/app/src/pages/history.tsx
@@ -17,23 +17,16 @@ const HomePageScrollable: NextPage = () => {
     includeResponses: true,
   });
   const [SelectedTile, setSelectedTile] = useState<string>("");
-  const [siblingTiles, setSiblingTiles] = useState({
-    top: "",
-    topLeft: "",
-    left: "",
-    right: "",
-    topRight: "",
-    bottom: "",
-    bottomLeft: "",
-    bottomRight: "",
-  });
   const [gridTiles, setGridTiles] = useState<string[]>([]);
 
-  console.log(siblingTiles);
   // const [pixels, setPixels] = useState<Pixels | undefined>(undefined);
   // const [riffLoading, setRiffLoading] = useState<boolean>(false);
 
   const { data: dailyCanvases, fetching } = canvasResults;
+
+  const handleTileClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setSelectedTile(event.currentTarget.name);
+  };
 
   const dailyCanvasResponses = useMemo(() => {
     let responses: CanvasResponse[] = [];
@@ -53,87 +46,39 @@ const HomePageScrollable: NextPage = () => {
     ));
   }, [dailyCanvasResponses]);
 
-  console.log(dailyCanvasResponses.length);
-
-  // if tile is in the left / center / right positon
-  // idsOfGrid = [tileIndex - 3, tileIndex - 2, tileIndex - 1, tileIndex, tileIndex + 1, tileIndex + 2, tileIndex + 3, tileIndex + 4, tileIndex + 5].reverse();
-
-  // center
-  // idsOfGrid = [tileIndex - 4, tileIndex - 3, tileIndex - 2, tileIndex - 1, tileIndex, tileIndex + 1, tileIndex + 2, tileIndex + 3, tileIndex + 4].reverse();
-  // if the tile
-
-  // calculate sibling tile index values from array of objects on 3-column grid
   useEffect(() => {
-    const totalTiles = dailyCanvasResponses.length;
-    // const columnCount = calc(totalTiles);
-    const columnCount = COLUMN_COUNT;
-    const rowCount = Math.ceil(totalTiles / columnCount);
     const selectedTileIndex = dailyCanvasResponses.findIndex(
       (canvasPrompt) => canvasPrompt.id === SelectedTile
     );
-    const selectedTileRow = Math.floor(selectedTileIndex / columnCount);
-    const selectedTileColumn = selectedTileIndex % columnCount;
-    const siblingTilesIndex = {
-      top: dailyCanvasResponses[selectedTileIndex - columnCount]?.id || "",
-      topRight:
-        dailyCanvasResponses[selectedTileIndex - columnCount - 1]?.id || "",
-      right: dailyCanvasResponses[selectedTileIndex - 1]?.id || "",
-      left:
-        selectedTileColumn == 0 || selectedTileColumn == 2
-          ? dailyCanvasResponses[selectedTileIndex + 1]?.id || ""
-          : "",
-      topLeft:
-        dailyCanvasResponses[selectedTileIndex - columnCount + 1]?.id || "",
-      bottom: dailyCanvasResponses[selectedTileIndex + columnCount]?.id || "",
-      bottomRight:
-        dailyCanvasResponses[selectedTileIndex + columnCount - 1]?.id || "",
-      bottomLeft:
-        dailyCanvasResponses[selectedTileIndex + columnCount + 1]?.id || "",
-    };
+    const selectedTileColumn = selectedTileIndex % COLUMN_COUNT;
 
-    setSiblingTiles(siblingTilesIndex);
-
-    // derive range of ids of row as well as rows above and below in 3-column grid
-
+    // Regardless of which column you click, we reference the center tile
+    // in the clicked row.
+    //
+    // [2, 1, 0]
+    // [2, 1, 0]
+    // [2, 1, 0]
+    //
+    // Example: Clicking column 2, will subtract 1 to center to 1,
+    // Clicking column 1 will add 1 to center to 1.
     let selectedTileIndexCenter;
-    // selectedTileIndex += 1;
-
-    if (selectedTileColumn == 1) {
-      selectedTileIndexCenter = selectedTileIndex;
+    if (selectedTileColumn == 2) {
+      selectedTileIndexCenter = selectedTileIndex - 1;
     } else if (selectedTileColumn == 0) {
       selectedTileIndexCenter = selectedTileIndex + 1;
     } else {
       selectedTileIndexCenter = selectedTileIndex;
     }
 
+    // Adds offsets based on column count
     const ids = dailyCanvasResponses.slice(
-      selectedTileIndexCenter - 4,
-      selectedTileIndexCenter + 4
+      selectedTileIndexCenter - (COLUMN_COUNT + Math.floor(COLUMN_COUNT / 2)),
+      selectedTileIndexCenter + (COLUMN_COUNT + Math.ceil(COLUMN_COUNT / 2))
     );
-    setGridTiles(ids.map((response) => response.id).sort());
 
-    // 376 - 3 = 373 + 0 = 373 = topLeft
-    // gridTiles.push(
-    //     dailyCanvasResponses[
-    //       selectedTileIndex - (columnCount + selectedTileColumn)
-    //     ]
-    //   );
-    //   gridTiles
-    //     .push
-    //     // dailyCanvasResponses[selectedTileIndex - (columnCount - 1 + selectedCol)]
-    //     ();
-    //   gridTiles.push(dailyCanvasResponses[selectedTileIndex - columnCount - 2]);
-    //   gridTiles.push(dailyCanvasResponses[selectedTileIndex]);
-    //   gridTiles.push(dailyCanvasResponses[selectedTileIndex + columnCount]);
-    //   gridTiles.push(dailyCanvasResponses[selectedTileIndex + columnCount + 1]);
-    //   gridTiles.push(dailyCanvasResponses[selectedTileIndex + columnCount + 2]);
+    const tiles = ids.map((response) => response.id).sort();
+    setGridTiles(tiles);
   }, [dailyCanvasResponses, SelectedTile]);
-
-  const siblingTilesReversed = Object.fromEntries(
-    Object.entries(siblingTiles).map(([key, value]) => [value, key])
-  );
-  console.log("reversed", siblingTilesReversed);
-  console.log("grid", gridTiles);
 
   return canvasResults && dailyCanvases && !fetching ? (
     <div className="flex justify-center w-full text-white pt-4">
@@ -145,28 +90,26 @@ const HomePageScrollable: NextPage = () => {
         >
           +
         </div>
-        {dailyCanvasResponses.map((c) => (
+        {dailyCanvasResponses.map((c, index) => (
           <button
-            style={{
-              position: "relative",
-              opacity: gridTiles.includes(c.id) ? "0.1" : "1",
-            }}
-            onClick={() => {
-              setSelectedTile(c.id);
-            }}
+            className="relative flex justify-center items-center"
+            onClick={handleTileClick}
+            name={c.id}
             key={c.id + "canvas"}
           >
-            <span
-              style={{
-                backgroundColor: "black",
-                color: "white",
-                position: "absolute",
-              }}
-            >
+            <span className="absolute white bg-slate-900 p-2 text-xs">
               {/* ts-ignore */}
-              {c.id}
+              id: {c.id} <br /> index: {index} <br /> column:{" "}
+              {index % COLUMN_COUNT}
             </span>
-            <SVG src={c.svg} width={160} height={160}></SVG>
+            <SVG
+              style={{
+                opacity: gridTiles.includes(c.id) ? 0.1 : 1,
+              }}
+              src={c.svg}
+              width={160}
+              height={160}
+            ></SVG>
           </button>
         ))}
       </div>
